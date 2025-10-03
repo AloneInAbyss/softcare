be13e6e0387dabe680bcbb3e9586d8f05b74ef9c
Thiago Assi <thiago-assi@outlook.com>
2025-10-03 11:51:08 -0300

    initial commit

---
+++ src/main/java/br/com/fiap/softcare/SoftcareApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.softcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftcareApplication.class, args);
+	}
+
+}

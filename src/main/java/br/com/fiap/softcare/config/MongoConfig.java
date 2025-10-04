1f80d893047eb496452cd6d432d753d5ff999472
Thiago Assi <thiago-assi@outlook.com>
2025-10-03 20:10:53 -0400

    fix warning String NonNull

--- src/main/java/br/com/fiap/softcare/config/MongoConfig.java
+++ src/main/java/br/com/fiap/softcare/config/MongoConfig.java
@@ -3,12 +3,14 @@ package br.com.fiap.softcare.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.lang.NonNull;
 
 @Configuration
 @EnableMongoRepositories(basePackages = "br.com.fiap.softcare.repository")
 public class MongoConfig extends AbstractMongoClientConfiguration {
 
     @Override
+    @NonNull
     protected String getDatabaseName() {
         return "softcare";
     }
